3521c35a3109826fcfd3937d53629a42fb39f93f
oswald-pro <oswaldmboussa1@gmail.com>
2022-11-10 09:57:20 +0100

    commit

--- Django1/django002/django002/urls.py
+++ Django1/django002/django002/urls.py
@@ -1,7 +1,7 @@
-"""django001 URL Configuration
+"""django002 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from demoApp import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('africa.urls'))
+    path("admin/", admin.site.urls),
+    path('', views.index, name='index'),
 ]
