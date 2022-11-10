fc27bccf136038a839af7ef43549c28b1262b7c4
oswald-pro <oswaldmboussa1@gmail.com>
2022-11-10 13:52:23 +0100

    commit

--- Login-Registration/authentication/views.py
+++ Login-Registration/authentication/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    return render(request, "userAuth/index.html")
+
+
+def login(request):
+    return render(request, "userAuth/login.html")
+
+
+def signup(request):
+    return render(request, "userAuths/signup.html")
+
+
+def logout(request):
+    pass
