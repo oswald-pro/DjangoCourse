538482d581bcb42340d9a0b2b5a070f06d2ea321
oswald-pro <oswaldmboussa1@gmail.com>
2022-11-10 17:27:20 +0100

    commit

--- AuthenticationApp/myproject/authentication/views.py
+++ AuthenticationApp/myproject/authentication/views.py
@@ -3,19 +3,19 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'UserAuth/index.html')
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'UserAuth/home.html')
 
 
 def signup(request):
-    return render(request, 'signup.html')
+    return render(request, 'UserAuth/signup.html')
 
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'UserAuth/login.html')
 
 
 def logout(request):
